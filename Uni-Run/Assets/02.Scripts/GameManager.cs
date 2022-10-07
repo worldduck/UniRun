25fc173d48b5fbb37c85e6fee1495e3492e17bee
worldduck <tonyskh0803@gmail.com>
2022-10-07 16:29:12 +0900

    20221007
    게임매니저

---
+++ Uni-Run/Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+// 게임 오버 상태를 표현하고, 게임 점수와 UI를 관리하는 게임 매니저
+// 씬에는 단 하나의 게임 매니저만 존재할 수 있음
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; // 싱글턴을 할당할 전역 변수
+
+    public bool isGameover = false; // 게임오버 상태
+    public Text scoreText; // 점수를 출력할 UI 텍스트
+    public GameObject gameoverUI;
+    // 게임 오버 시 활성화할 UI게임 오브젝트
+
+    private int score = 0; // 게임 점수
+
+    // 게임 시작과 동시에 싱글턴을 구성
+    void Awake()
+    {
+        // 싱글턴 변수 instance가 비어 있는가?
+        if(instance == null)
+        {
+            // instance가 비어있다면(null) 그곳에 자기 자신을 할당
+            instance = this;
+        }
+        else
+        {
+            // instance에 이미 다른 GameManager 오브젝트가 할당되어 있는 경우
+
+            // 씬에 두 개 이상의 GameManager 오브젝트가 존재한다는 의미
+            // 싱글턴 오브젝트는 하나만 존재해야 하므로
+            // 자신의 게임 오브젝트를 파괴
+            Debug.LogWarning("씬에 두 개 이상의 게임 매니저가 존재합니다!");
+            Destroy(gameObject);
+        }
+    }
+   
+    void Update()
+    {
+       if (isGameover && Input.GetMouseButtonDown(0))
+        {
+            // 게임오버 상태에서 마우스 왼쪽 버튼을 클릭하면
+            // 현재 씬 재시작
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        } 
+    }
+
+    // 점수를 증가 시키는 메서드
+    public void AddScore(int newScore)
+    {
+        // 게임오버가 아니라면
+        if (!isGameover)
+        {
+            // 점수를 증가
+            score += newScore;
+            scoreText.text = "Score : " + score;
+        }
+    }
+
+    // 플레이어 캐릭터 사망 시 게임오버를 실행하는 메서드
+    public void OnPlayerDead()
+    {
+        isGameover = true;
+        gameoverUI.SetActive(true);
+    }
+    
+}
--- Uni-Run/Assets/02.Scripts/ScrollingObject.cs
+++ Uni-Run/Assets/02.Scripts/ScrollingObject.cs
@@ -9,9 +9,11 @@ public class ScrollingObject : MonoBehaviour
     
     void Update()
     {
-        // 초당 speed의 속도로 왼쪽으로 평행이동
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
-        //transform.position -= transform.right * speed * Time.deltaTime;
-        
+        // 게임오버가 아니라면
+        if (!GameManager.instance.isGameover)
+        {
+            // 초당 speed의 속도로 왼쪽으로 평행이동
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
