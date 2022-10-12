e4de670dce7472fef3b9b13412d59d9848302e30
worldduck <tonyskh0803@gmail.com>
2022-10-12 16:44:19 +0900

    20221012
    유니런 완성

--- Uni-Run/Assets/02.Scripts/PlatformSpawner.cs
+++ Uni-Run/Assets/02.Scripts/PlatformSpawner.cs
@@ -17,11 +17,11 @@ public class PlatformSpawner : MonoBehaviour
     private float xPos = 20f; // 배치할 위치의 x 값
 
     private GameObject[] platforms; // 미리 생성한 발판들
-    private float currentIndex = 0; // 사용할 현재 순번의 발판
+    private int currentIndex = 0; // 사용할 현재 순번의 발판
 
     // 초반에 생성한 발판을 화면 밖에 숨겨둘 위치
     private Vector2 poolPosition = new Vector2(0, -25);
-    private float lastSpqwnTime; // 마지막 배치 시점
+    private float lastSpawnTime; // 마지막 배치 시점
     
     void Start()
     {
@@ -31,12 +31,55 @@ public class PlatformSpawner : MonoBehaviour
         // count만큼 루프하면서 발판 생성
         for (int i = 0; i<count; i++)
         {
-
+            // platformPrefab을 원본으로 새 발판을 poolPosittion 위치에 복제 생성
+            // 생성된 발판을 platform 배열에 할당
+            platforms[i] 
+                = Instantiate(platformPrefab, poolPosition, Quaternion.identity);
         }
+
+        // 마지막 배치 시점 초기화
+        lastSpawnTime = 0f;
+        // 다음번 배치까지의 시간 간격을 0으로 초기화
+        timeBetSpawn = 0f;
     }
 
     void Update()
     {
-        // 순서를 돌아가며 주기적으로 발판을 배치
+        // 게임오버 상태에서는 동작하지 않음
+        if (GameManager.instance.isGameover)
+        {
+            return;
+        }
+
+        // 마지막 배치 시점에서 timeBetSpawn 이상 시간이 흘렀다면
+        if (Time.time >= lastSpawnTime + timeBetSpawn)
+        {
+            // 기록된 마지막 배치 시점을 현재 시점으로 갱신
+            lastSpawnTime = Time.time;
+
+            // 다음 배치까지의 시간 간격을
+            // timeBetspawnMin, timeBetspawnMax 사이에서 랜덤 설정
+            timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
+
+            // 배치할 위치의 높이를 yMin과 yMax 사이에서 랜덤 설정
+            float yPos = Random.Range(yMin, yMax);
+
+            // 사용할 현재 순번의 발판 게임 오브젝트를 비활성화하고
+            // 즉시 다시 활성화
+            // 이때 발판의 Platform 컴포넌트의 OnEnable 메서드가 실행
+            platforms[currentIndex].SetActive(false);
+            platforms[currentIndex].SetActive(true);
+
+            // 현재 순번의 발판을 화면 오른쪽에 재배치
+            platforms[currentIndex].transform.position = new Vector2(xPos, yPos);
+            // 순번 넘기기
+            currentIndex++;
+
+            // 마지막 순번에 도달했다면 순번을 리셋
+            if (currentIndex >= count)
+            {
+                currentIndex = 0;
+            }
+        }
     }
 }
